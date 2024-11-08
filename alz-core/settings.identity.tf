a248f930bb504a28d28182dbbb0adc7c230291b5
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-08 14:46:02 -0300

    update settings.identity.tf: enable public IP and RDP denial, add advanced identity configuration

--- alz-core/settings.identity.tf
+++ alz-core/settings.identity.tf
@@ -4,10 +4,21 @@ locals {
       identity = {
         enabled = true
         config = {
-          enable_deny_public_ip             = false
-          enable_deny_rdp_from_internet     = false
+          enable_deny_public_ip             = true
+          enable_deny_rdp_from_internet     = true
           enable_deny_subnet_without_nsg    = false
           enable_deploy_azure_backup_on_vms = false
+        }
+      }
+    }
+  advanced = {
+      resource_prefix = "acn"
+      resource_suffix = "prd-cace"
+      custom_settings_by_resource_type = {
+        azurerm_resource_group = {
+          identity = {
+            name = "acn-identity-prd-cace-rg-01"
+          }
         }
       }
     }
