efe630171860c8422c9fb778e9b3904fa01e4777
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-08 16:42:45 -0300

    Add connectivity resources configuration and update variables

--- alz-core/main.tf
+++ alz-core/main.tf
@@ -60,4 +60,8 @@ module "enterprise_scale" {
   deploy_identity_resources    = true
   subscription_id_identity     = data.azurerm_client_config.core.subscription_id # replace is Identity Subscription ID
   configure_identity_resources = local.configure_identity_resources
+
+  deploy_connectivity_resources    = true
+  subscription_id_connectivity     = data.azurerm_client_config.core.subscription_id # replace is Connectivity Subscription ID
+  configure_connectivity_resources = local.configure_connectivity_resources
 }
